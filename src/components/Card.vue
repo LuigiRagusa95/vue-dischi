489032f1992bf6833eb8542abb38948faf7b1279
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-25 15:54:31 +0100

    ADD: add grid and card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,99 @@
 <template>
-	<div class="card">ciao</div>
+	<div class="card">
+		<div class="card-image">
+			<img :src="poster" alt="poster-image" />
+		</div>
+		<div class="card-info">
+			<div class="card-info-title">
+				<h4>{{ title }}</h4>
+			</div>
+			<span class="card-info-author">{{ author }}</span>
+			<span class="card-info-year">{{ year }}</span>
+			<span class="card-info-genre">{{ genre }}</span>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
 	name: "Card",
+	props: {
+		poster: String,
+		title: String,
+		author: String,
+		genre: String,
+		year: String,
+	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+	display: flex;
+	padding: 1rem;
+	margin: 0.5rem;
+	flex-flow: column;
+	cursor: pointer;
+	border-radius: 3px;
+	transition: 0.2s ease;
+	width: calc((100% / 6) - 1rem);
+	background: rgba($color: #ffffff, $alpha: 0.05);
+	box-shadow: 0 2px 8px rgba($color: #000000, $alpha: 0.2);
+
+	&:hover {
+		background: rgba($color: #ffffff, $alpha: 0.1);
+	}
+
+	&-image {
+		width: 100%;
+		display: flex;
+		overflow: hidden;
+		position: relative;
+		padding-bottom: calc(100% * 1);
+
+		> img {
+			top: 0;
+			left: 0;
+			width: 100%;
+			position: absolute;
+			object-fit: contain;
+		}
+	}
+
+	&-info {
+		display: flex;
+		margin-top: 1rem;
+		text-align: center;
+		flex-direction: column;
+
+		&-title {
+			width: 100%;
+			display: flex;
+			min-width: 0;
+			justify-content: center;
+
+			> h4 {
+				overflow: hidden;
+				color: #f5f5f5;
+				font-size: 1.25rem;
+				line-height: 2.5rem;
+				white-space: nowrap;
+				text-overflow: ellipsis;
+			}
+		}
+
+		&-author,
+		&-year,
+		&-genre {
+			font-size: 0.875rem;
+			color: #ffffff40;
+			line-height: 1.5rem;
+		}
+
+		&-genre {
+			color: #ffffff;
+			font-size: 1rem;
+		}
+	}
+}
+</style>
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,7 +1,9 @@
 <template>
 	<section>
 		<div class="container">
-			<Card />
+			<template v-for="(album, index) in list">
+				<Card :key="`card-${index}`" :poster="album.poster" :title="album.title" :author="album.author" :year="album.year" :genre="album.genre" />
+			</template>
 		</div>
 	</section>
 </template>
@@ -13,15 +15,37 @@ import Card from "./Card.vue";
 export default {
 	components: { Card },
 	name: "Grid",
+	data() {
+		return {
+			list: null,
+		};
+	},
+	methods: {
+		getData() {
+			axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((response) => {
+				this.list = response.data.response;
+			});
+		},
+	},
+	created() {
+		this.getData();
+	},
 };
 </script>
 
 <style lang="scss" scoped>
-.container {
+section {
 	display: flex;
-	padding: 10px;
-	margin: 0 auto;
-	flex-wrap: wrap;
-	max-width: 1400px;
+
+	& .container {
+		flex-grow: 1;
+		display: flex;
+		padding: 10px;
+		margin: 0 auto;
+		flex-wrap: wrap;
+		max-width: 1400px;
+		align-items: center;
+		justify-content: center;
+	}
 }
 </style>
