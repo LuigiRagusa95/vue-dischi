3f8be3113753f4af3c8d8fd7a257d857dc0c0833
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-25 16:08:12 +0100

    ADD: add Loader component

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,9 +1,12 @@
 <template>
 	<section>
 		<div class="container">
-			<template v-for="(album, index) in list">
-				<Card :key="`card-${index}`" :poster="album.poster" :title="album.title" :author="album.author" :year="album.year" :genre="album.genre" />
-			</template>
+			<div class="container-box" v-if="list">
+				<template v-for="(album, index) in list">
+					<Card :key="`card-${index}`" :poster="album.poster" :title="album.title" :author="album.author" :year="album.year" :genre="album.genre" />
+				</template>
+			</div>
+			<Loader v-else />
 		</div>
 	</section>
 </template>
@@ -12,8 +15,9 @@
 import axios from "axios";
 
 import Card from "./Card.vue";
+import Loader from "./Loader.vue";
 export default {
-	components: { Card },
+	components: { Card, Loader },
 	name: "Grid",
 	data() {
 		return {
@@ -22,9 +26,11 @@ export default {
 	},
 	methods: {
 		getData() {
-			axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((response) => {
-				this.list = response.data.response;
-			});
+			setTimeout(() => {
+				axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((response) => {
+					this.list = response.data.response;
+				});
+			}, 2000);
 		},
 	},
 	created() {
@@ -36,16 +42,24 @@ export default {
 <style lang="scss" scoped>
 section {
 	display: flex;
+	flex-direction: column;
 
 	& .container {
-		flex-grow: 1;
 		display: flex;
-		padding: 10px;
-		margin: 0 auto;
-		flex-wrap: wrap;
-		max-width: 1400px;
+		min-height: 100vh;
 		align-items: center;
 		justify-content: center;
+
+		&-box {
+			flex-grow: 1;
+			display: flex;
+			padding: 10px;
+			margin: 0 auto;
+			flex-wrap: wrap;
+			max-width: 1400px;
+			align-items: center;
+			justify-content: center;
+		}
 	}
 }
 </style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="screen">
+		<div class="loader"></div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.loader {
+	width: 24px;
+	height: 24px;
+	display: block;
+	border-radius: 999999px;
+	border: 3px solid #55d56110;
+	border-right-color: #55d561;
+	animation: spin 500ms linear infinite;
+
+	@keyframes spin {
+		to {
+			transform: rotate(1turn);
+		}
+	}
+}
+</style>
