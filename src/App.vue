621e96fae6fda99bd305f7d00af11185887c4aab
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-25 14:55:00 +0100

    ADD: add header component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
 	<div id="app">
-    
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
 export default {
 	name: "App",
-	components: {},
+	components: {Header},
 };
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,34 @@
-<template></template>
+<template>
+	<header>
+		<div class="logo">
+			<a href="#"><img src="../assets/logo.png" alt="brand-logo" /></a>
+		</div>
+	</header>
+</template>
 
 <script>
-export default {};
+export default {
+	name: "Header",
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+	padding: 5px 10px;
+	background: #2e3a46;
+}
+
+header .logo {
+	width: 3rem;
+	height: 3rem;
+	display: flex;
+	overflow: hidden;
+	align-items: center;
+	justify-content: center;
+}
+
+header .logo > a > img {
+	width: 100%;
+	object-fit: contain;
+}
+</style>
